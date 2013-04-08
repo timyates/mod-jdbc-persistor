0c73496a69ba12e3a6ba6a2a77ec4f1f191bde74
Tim Yates <tim.yates@gmail.com>
2013-04-08 14:48:14 +0100

    Whoops...  Fixed timestamp test

--- src/test/resources/integration_tests/javascript/test_client.js
+++ src/test/resources/integration_tests/javascript/test_client.js
@@ -165,28 +165,28 @@ function testTimestamp() {
                 [ '2013-02-15 12:00:00' ],
                 [ '2013-02-16 11:00:00' ] ]
     }, function( reply ) {
-      tu.azzert( reply.status === 'ok' ) ;
-      tu.azzert( reply.result != undefined ) ;
-      tu.azzert( reply.result.length == 3 ) ;
-      tu.azzert( reply.result[ 0 ].ID == 1 ) ;
-      tu.azzert( reply.result[ 1 ].ID == 2 ) ;
-      tu.azzert( reply.result[ 2 ].ID == 3 ) ;
+      vassert.assertEquals( reply.status, 'ok' ) ;
+      vassert.assertNotSame( reply.result, undefined ) ;
+      vassert.assertEquals( reply.result.length, 3, 0 ) ;
+      vassert.assertEquals( reply.result[ 0 ].ID, 1, 0 ) ;
+      vassert.assertEquals( reply.result[ 1 ].ID, 2, 0 ) ;
+      vassert.assertEquals( reply.result[ 2 ].ID, 3, 0 ) ;
       eb.send( 'test.persistor', {
         action: 'select',
         stmt:   "SELECT id, TO_CHAR( time, 'YYYY-MM-DD HH24:MI:SS' ) AS TIME FROM test ORDER BY ID ASC"
       }, function( reply ) {
-        tu.azzert( reply.status === 'ok' ) ;
-        tu.azzert( reply.result.length == 3 ) ;
-        tu.azzert( reply.result[ 0 ].ID   === 1 ) ;
-        tu.azzert( reply.result[ 0 ].TIME === '2013-02-14 12:30:44', reply.result[ 0 ].TIME ) ;
+        vassert.assertEquals( reply.status, 'ok' ) ;
+        vassert.assertEquals( reply.result.length, 3, 0 ) ;
+        vassert.assertEquals( reply.result[ 0 ].ID, 1, 0 ) ;
+        vassert.assertEquals( reply.result[ 0 ].TIME, '2013-02-14 12:30:44' ) ;
 
-        tu.azzert( reply.result[ 1 ].ID   === 2 ) ;
-        tu.azzert( reply.result[ 1 ].TIME === '2013-02-15 12:00:00' ) ;
+        vassert.assertEquals( reply.result[ 1 ].ID, 2, 0 ) ;
+        vassert.assertEquals( reply.result[ 1 ].TIME, '2013-02-15 12:00:00' ) ;
 
-        tu.azzert( reply.result[ 2 ].ID   === 3 ) ;
-        tu.azzert( reply.result[ 2 ].TIME === '2013-02-16 11:00:00' ) ;
+        vassert.assertEquals( reply.result[ 2 ].ID, 3, 0 ) ;
+        vassert.assertEquals( reply.result[ 2 ].TIME, '2013-02-16 11:00:00' ) ;
 
-        tu.testComplete() ;
+        vassert.testComplete() ;
       } ) ;
     } ) ;
   } )
